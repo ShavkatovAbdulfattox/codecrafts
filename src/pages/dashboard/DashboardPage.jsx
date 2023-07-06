bb46134c39791aa7d269cad910b549e4fd8f66b8
Abdulfattox <vwhoami@yahoo.com>
2023-07-06 23:29:42 +0500

    start component is added

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,13 +1,12 @@
-import Start from "../../components/Start/Start"
+import Start from "../../components/Start/Start";
 
 const DashboardPage = () => {
-     return (
-          <main>
-                    
-               <Start/>
-               DashboardPage
-          </main>
-     )
-
+  return (
+    <main>
+      <Start />
+      DashboardPage
+    </main>
+  );
+};
 
 export default DashboardPage;
