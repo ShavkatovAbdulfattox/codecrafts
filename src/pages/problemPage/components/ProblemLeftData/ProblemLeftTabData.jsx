eab6fec42f55e718cfc4c763f0cd0988d851e85b
Sukhrob Abdullaev <sukhrobjonabdullaev3365@gmail.com>
2023-08-17 13:43:54 +0500

    fixed leftSide

--- src/pages/problemPage/components/ProblemLeftData/ProblemLeftTabData.jsx
+++ src/pages/problemPage/components/ProblemLeftData/ProblemLeftTabData.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable react/prop-types */
 import { Select } from "antd";
-import { useState, useEffect } from "react";
+import { useState, useEffect, memo } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
-// import Tab3Table from "./Tab3Table";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 const Tab3 = () => {
   const [data, setData] = useState([]);
@@ -50,7 +59,7 @@ const Tab3 = () => {
 
   return (
     <>
-      <div className="">
+      {/* <div className="">
         <ul className="flex justify-between items-center">
           <li>
             <Select
@@ -60,7 +69,7 @@ const Tab3 = () => {
               }}
               onChange={handleChange}
               options={uniqueStatusValues.map((status) => ({
-                value: status, // Ensure each option has a unique value
+                value: status, 
                 label: status,
               }))}
               style={{
@@ -76,7 +85,7 @@ const Tab3 = () => {
               }}
               onChange={handleChangeLang}
               options={uniqueStatusValuesLang.map((lang) => ({
-                value: lang, // Ensure each option has a unique value
+                value: lang, 
                 label: lang,
               }))}
               style={{
@@ -92,8 +101,7 @@ const Tab3 = () => {
           </li>
         </ul>
         <ul className="flex justify-between items-center">
-          {/* satatus options */}
-          <li>
+          <Link>
             <div className="flex flex-col py-[9px]">
               {data.map((el) => (
                 <div
@@ -108,9 +116,8 @@ const Tab3 = () => {
                 </div>
               ))}
             </div>
-          </li>
-          {/* language options */}
-          <li>
+          </Link>
+          <Link>
             <div className="lang flex flex-col py-[9px]">
               {data.map((el) => (
                 <div key={el.id} className="px-2 py-[9px]">
@@ -118,8 +125,8 @@ const Tab3 = () => {
                 </div>
               ))}
             </div>
-          </li>
-          <li className="pt-2 cursor-pointer hover:bg-[#ffffff12]">
+          </Link>
+          <Link className="pt-2 hover:bg-[#ffffff12]">
             <div className="runtime py-[9px]">
               {data.map((el) => (
                 <div key={el.id} className="px-2 py-[9px]">
@@ -127,8 +134,8 @@ const Tab3 = () => {
                 </div>
               ))}
             </div>
-          </li>
-          <li className="pt-2 cursor-pointer hover:bg-[#ffffff12]">
+          </Link>
+          <Link className="pt-2 hover:bg-[#ffffff12]">
             <div className="time py-[9px]">
               {data.map((el) => (
                 <div key={el.id} className="px-2 py-[9px]">
@@ -139,12 +146,133 @@ const Tab3 = () => {
                 </div>
               ))}
             </div>
-          </li>
+          </Link>
         </ul>
-      </div>
-      {/* <Tab3Table/> */}
+      </div> */}
+      {/* <CustomTable /> */}
+      <TableContainer
+        component={Paper}
+        sx={{
+          backgroundColor: "transparent",
+          boxShadow: "none",
+        }}
+      >
+        <Table aria-label="custom table">
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{
+                  borderBottomColor: "#f7faff2e",
+                }}
+              >
+                <Select
+                  defaultValue={{
+                    value: "status",
+                    label: "Status",
+                  }}
+                  onChange={handleChange}
+                  options={uniqueStatusValues.map((status) => ({
+                    value: status,
+                    label: status,
+                  }))}
+                  style={{
+                    width: 90,
+                  }}
+                />
+              </TableCell>
+              <TableCell sx={{ borderBottomColor: "#f7faff2e" }}>
+                <Select
+                  defaultValue={{
+                    value: "langauge",
+                    label: "Language",
+                  }}
+                  onChange={handleChangeLang}
+                  options={uniqueStatusValuesLang.map((lang) => ({
+                    value: lang,
+                    label: lang,
+                  }))}
+                  style={{
+                    width: 90,
+                  }}
+                />
+              </TableCell>
+              <TableCell
+                sx={{
+                  borderBottomColor: "#f7faff2e",
+                   color: '#eff1f6bf'
+                }}
+              >
+                <button>Runtime</button>
+              </TableCell>
+              <TableCell
+                sx={{
+                  borderBottomColor: "#f7faff2e",
+                   color: '#eff1f6bf'
+                }}
+              >
+                <button>Time</button>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((el) => (
+              <TableRow
+                key={el.id}
+                component={RouterLink}
+                to={`/problem/2/${el.id}`} // Replace with your route structure
+                sx={{
+                  textDecoration: "none",
+                  "&:hover": { backgroundColor: "#ffffff12" },
+                }}
+              >
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{
+                    borderBottom: "none",
+                    color: (theme) =>
+                      el.status === "ERROR"
+                        ? theme.palette.error.main
+                        : theme.palette.success.main,
+                  }}
+                >
+                  {el.status}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    borderBottom: "none",
+                    color: '#eff1f6bf'
+                  }}
+                >
+                  {el.language}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    borderBottom: "none",
+                     color: '#eff1f6bf'
+                  }}
+                >
+                  {" "}
+                  {`${el.runtime} ms`}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    borderBottom: "none",
+                     color: '#eff1f6bf'
+                  }}
+                >
+                  {`${new Date(el.time)
+                    .toLocaleString("en-US", { month: "short" })
+                    .toLowerCase()} ${new Date(el.time).getDate()}, 
+                  ${new Date(el.time).getFullYear()}`}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
 
-export default Tab3;
+export default memo(Tab3);
