44b56e0391ebbb669d4089db17ca94aa0d027609
= <=>
2023-08-22 23:47:33 +0500

    l

--- src/pages/postSolution/PostSolution.jsx
+++ src/pages/postSolution/PostSolution.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { useState } from "react";
 
 import SolutionHeader from "./components/solutionHeader/SolutionHeader";
@@ -7,43 +9,46 @@ import "./postSolution.scss";
 import { usePostSolutionMutation } from "../../services/questionApi";
 
 const PostSolution = () => {
-  const user = localStorage.getItem("userData");
+    const user = localStorage.getItem("userData");
 
-  const [response, setResponse] = useState({
-    name: "",
-    text: "",
-    tags: [],
-    userId: JSON.parse(user)?.data?.id,
-    questionId: 3,
-    queryId: 0,
-  });
+    const [response, setResponse] = useState({
+        name: "",
+        text: "",
+        tags: [],
+        userId: JSON.parse(user)?.data?.id,
+        questionId: 3,
+        queryId: 0,
+    });
 
-  const [postSolution, { isLoading, data }] = usePostSolutionMutation();
+    const [postSolution, { isLoading }] = usePostSolutionMutation();
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
 
-  const handleClick = () => {
-    postSolution(response).then((res) => res);
-  };
+    const handleClick = () => {
+        postSolution(response).then((res) => res);
+    };
 
-  return (
-    <div className="post-solution">
-      <div className="post-solution__container">
-        <div className="post-solution__header">
-          <SolutionHeader
-            handleClick={handleClick}
-            response={response}
-            setResponse={setResponse}
-          />
+    return (
+        <div className="post-solution">
+            <div className="post-solution__container">
+                <div className="post-solution__header">
+                    <SolutionHeader
+                        handleClick={handleClick}
+                        response={response}
+                        setResponse={setResponse}
+                    />
+                </div>
+                <div className="post-solution__body">
+                    <SolutionBody
+                        response={response}
+                        setResponse={setResponse}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="post-solution__body">
-          <SolutionBody response={response} setResponse={setResponse} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PostSolution;
--- src/pages/problemPage/components/ProblemLeftSide.jsx
+++ src/pages/problemPage/components/ProblemLeftSide.jsx
@@ -1,81 +1,143 @@
-/* eslint-disable react/prop-types */
+/**
+ * eslint-disable react/prop-types
+ *
+ * @format
+ */
+
 import { Tabs, Tag } from "antd";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { CorrectIcon, DislikeButton, LikeButton } from "../../../utils/icons";
 import { getQuestionDifficulty } from "../../../utils/functions";
 import ProblemLeftExampleCard from "./ProblemLeftExampleCard";
 
 // problem data
 import Tab3 from "./ProblemLeftData/ProblemLeftTabData";
+import { Resizable } from "re-resizable";
 
-const ProblemLeftSide = ({ question }) => {
-  const onChangeTab = (key) => {
-    // console.log(key);
-  };
+const ProblemLeftSide = ({ question = {} }) => {
+    const [rightWidth, setRightWidth] = useState(0);
 
-  const items = [
-    {
-      key: "1",
-      label: `Izoh`,
-      children: (
-        <div className="left-side__body">
-          <h1>{question.name}</h1>
-          <div className="left-side__info">
-            <Tag
-              color="orange"
-              style={{ borderRadius: "50px", padding: "2px 15px" }}
-            >
-              {getQuestionDifficulty(question.level)}
-            </Tag>
-            <CorrectIcon />
-            <button className="left-side__like">
-              <LikeButton />
-              <span>{question.like1}</span>
-            </button>
-            <button className="left-side__dislike">
-              <DislikeButton />
-              <span>{question.dislike}</span>
-            </button>
-          </div>
-          <div className="left-problem__text">{question.definition}</div>
-          <div className="left-problem__examples">
-            {question.exampleList?.map((example, index) => {
-              return (
-                <ProblemLeftExampleCard
-                  key={index}
-                  example={example}
-                  index={index}
-                />
-              );
-            })}
-          </div>
-        </div>
-      ),
-    },
-    {
-      key: "2",
-      label: `Boshqa yechimlar`,
-      children: `hey2`,
-    },
-    {
-      key: "3",
-      label: `Yuborigan javoblar`,
-      children: <Tab3 />,
-    },
-  ];
+    const onChangeTab = (key) => {
+        // console.log(key);
+    };
 
-  return (
-    <section className="left-side">
-      <div className="left-side__header">
-        <Tabs
-          size="small"
-          defaultActiveKey="1"
-          items={items}
-          onChange={onChangeTab}
-        />
-      </div>
-    </section>
-  );
+    const onResize = (a) => {
+        setRightWidth(a.screenX);
+    };
+
+    const items = [
+        {
+            key: "1",
+            label: `Izoh`,
+            children: (
+                <div className="left-side__body">
+                    <div className="left-side__body--container">
+                        <div className="left-side__body--box">
+                            <h1>{question.name}</h1>
+                            <div className="left-side__info">
+                                <Tag
+                                    color="orange"
+                                    style={{
+                                        borderRadius: "50px",
+                                        padding: "2px 15px",
+                                    }}
+                                >
+                                    {getQuestionDifficulty(question.level)}
+                                </Tag>
+                                <CorrectIcon />
+                                <button className="left-side__like">
+                                    <LikeButton />
+                                    <span>{question.like1}</span>
+                                </button>
+                                <button className="left-side__dislike">
+                                    <DislikeButton />
+                                    <span>{question.dislike}</span>
+                                </button>
+                            </div>
+                            <div className="left-problem__text">
+                                <pre>{question.definition?.trim()}</pre>
+                            </div>
+                            <div className="left-problem__examples">
+                                {question.exampleList?.map((example, index) => {
+                                    return (
+                                        <ProblemLeftExampleCard
+                                            key={index}
+                                            example={example}
+                                            index={index}
+                                        />
+                                    );
+                                })}
+                            </div>
+                            <div className="left-problem__examples">
+                                {question.exampleList?.map((example, index) => {
+                                    return (
+                                        <ProblemLeftExampleCard
+                                            key={index}
+                                            example={example}
+                                            index={index}
+                                        />
+                                    );
+                                })}
+                            </div>
+                            <div className="left-problem__examples">
+                                {question.exampleList?.map((example, index) => {
+                                    return (
+                                        <ProblemLeftExampleCard
+                                            key={index}
+                                            example={example}
+                                            index={index}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <Resizable
+                            className="playground-resizer"
+                            enable={{
+                                top: false,
+                                right: true,
+                                bottom: false,
+                                left: false,
+                                topRight: false,
+                                bottomRight: false,
+                                bottomLeft: false,
+                                topLeft: false,
+                            }}
+                            maxHeight="55%"
+                            onResize={onResize}
+                            defaultSize={{
+                                height: "40%",
+                                width: "100%",
+                            }}
+                        >
+                            <div className="left-side__submit">sUMOFINSD</div>
+                        </Resizable>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            key: "2",
+            label: `Boshqa yechimlar`,
+            children: `hey2`,
+        },
+        {
+            key: "3",
+            label: `Yuborigan javoblar`,
+            children: <Tab3 />,
+        },
+    ];
+
+    return (
+        <section className="left-side">
+            <Tabs
+                size="small"
+                defaultActiveKey="1"
+                items={items}
+                onChange={onChangeTab}
+            />
+        </section>
+    );
 };
 
 export default memo(ProblemLeftSide);
